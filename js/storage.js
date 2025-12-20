4ff328f6ac390de73129aff3dba9ad3c1190a97d
doflamingo-evil <sangeerthsanthoshik2233@gmail.com>
2025-12-20 10:06:53 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,13 @@
+const STORAGE_KEY = "solo-leveling-data";
 
+function saveData(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function loadData() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY));
+}
+
+function clearData() {
+  localStorage.removeItem(STORAGE_KEY);
+}
