45a9101bc787b6d3c75837e1b28b6df9cfa9cddf
doflamingo-evil <sangeerthsanthoshik2233@gmail.com>
2025-12-21 23:58:02 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+const screens = document.querySelectorAll(".screen");
+const modal = document.getElementById("authModal");
 
+function show(id) {
+  screens.forEach(s => s.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
+}
+
+loginBtn.onclick = () => modal.classList.remove("hidden");
+registerBtn.onclick = () => {
+  modal.classList.remove("hidden");
+  authTitle.textContent = "Create Account";
+};
+
+authNext.onclick = () => {
+  modal.classList.add("hidden");
+  show("basicInfo");
+};
+
+toStats.onclick = () => show("statsSetup");
+finishSetup.onclick = () => show("profile");
