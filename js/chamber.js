ccb668d13dfe8d22c1265cb7ee56faa62ab909dc
doflamingo-evil <sangeerthsanthoshik2233@gmail.com>
2026-01-04 13:08:17 +0530

    Update chamber.js

--- js/chamber.js
+++ js/chamber.js
@@ -1,1 +1,23 @@
+let hunter = JSON.parse(localStorage.getItem("hunter"));
 
+function openChamber(name) {
+  document.querySelectorAll(".chamber").forEach(c => c.classList.remove("active"));
+  document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
+
+  document.getElementById(name).classList.add("active");
+  event.target.classList.add("active");
+}
+
+function gainStatXP(stat, amount) {
+  hunter.stats[stat]++;
+  hunter.xp += amount;
+
+  localStorage.setItem("hunter", JSON.stringify(hunter));
+  alert(`SYSTEM: ${stat.toUpperCase()} increased`);
+}
+
+function completeQuest(xp) {
+  hunter.xp += xp;
+  localStorage.setItem("hunter", JSON.stringify(hunter));
+  alert("SYSTEM: QUEST COMPLETED");
+}
